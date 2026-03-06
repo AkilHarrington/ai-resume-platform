850a206c58ecf8f345bc5fe4437c31ef0402494f
Akil Harrington <akilharrington@gmail.com>
2026-03-06 18:42:30 -0400

    Update CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -12,12 +12,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "null",
-        "https://YOUR-FRONTEND-URL.onrender.com",
-    ],
+   allow_origins=[
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "null",
+    "https://ai-resume-platform-1-da6p.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
